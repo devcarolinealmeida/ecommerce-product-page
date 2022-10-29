5ab355af9494c0518db492a37d3f7009b572318f
devcarolinealmeida <carolineitalia85@gmail.com>
2022-10-29 18:47:53 +0200

    add colors and font-family to tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,16 @@ module.exports = {
     './index.html',
   ],
   theme: {
+    colors: {
+      'orange': rgb(255, 125, 26),
+      'oragne-light': 	rgb(255, 237, 224),
+      'dark-blue': rgb(29, 32, 37),
+      'grayish-blue': rgb(182, 188, 200),
+    },
+    fontFamily: {
+      sans: ['Kumbh Sans', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
     extend: {},
   },
   plugins: [],
